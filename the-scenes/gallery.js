5eee2bf5f466a67d821db65a082c2472d5538f6d
Yuzzi997 <zhuy700@newschool.edu>
2022-04-22 10:33:54 -0400

    trial 1

--- the-scenes/gallery.js
+++ the-scenes/gallery.js
@@ -1,23 +1,74 @@
 console.log("hello");
 
 var Airtable = require('airtable');
-console.log(Airtable);
+var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base('appKxL9aU5QyWGFe6');
 
-//use airtable library
-var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base(
-    'app8QITorsk2f2Pf2');
+base('scenes').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
 
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base('appKxL9aU5QyWGFe6');
-    
-    base('scenes').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-    
-        records.forEach(function(record) {
-            console.log('Retrieved', record.get('image'));
-        });
-    
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('image'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { 
+        console.error(err); 
+        return; }
+});
+
+/////////////////////
+
+//get the "books" table from the base, select ALL the records
+// specify the functions that will receive the data
+base("scenes").select({}).eachPage(gotPageOfScenes, gotAllScenes);
+
+// an empty array to hold our book data
+const scenes = [];
+
+
+
+// callback function that is used when all pages are loaded
+function gotAllScenes(err) {
+    console.log("gotAllScenes()");
+  
+    // report an error> this is what shows up if there's a problem
+    if (err) {
+      console.log("error loading scenes");
+      console.error(err);
+      return;
+    }
+  
+    // call functions to log and show the books
+    consoleLogScenes();
+    showScenes();
+  }
+
+
+function consoleLogScenes() {
+    console.log("consoleLogScenes()");
+    books.forEach((scene) => {
+      console.log("Scene:", image);
+    });
+  }
+
+
+// loop through the books, create an h2 for each one, and add it to the page
+function showScenes() {
+    console.log("showScenes()");
+    books.forEach((scene) => {
+      const h2 = document.createElement("h2");
+  
+//     try changing 'title' below to 'description' and your description will show instead of your title    
+h2.innerText = scenes.fields.image;
+document.body.appendChild(h2);
+});
+}
--- the-scenes/script.js
+++ the-scenes/script.js
@@ -1,26 +1,100 @@
-console.log("hello");
+/* globals require */
+console.log("Hello, Airtable");
 
 var Airtable = require('airtable');
-console.log(Airtable);
+var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base('appKxL9aU5QyWGFe6');
 
-//use airtable library
-var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base(
-    'app8QITorsk2f2Pf2');
+base('scenes').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
 
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base('appKxL9aU5QyWGFe6');
-    
-    base('scenes').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-    
-        records.forEach(function(record) {
-            console.log('Retrieved', record.get('image'));
-        });
-    
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Scenes'));
+    });
 
-       
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+// }, function done(err) {
+//     if (err) { console.error(err); return; }
+// });
+
+// // an empty array to hold our people data
+// let scenes = [];
+
+// callback function that receives each page of data (considered here as records) and adds them to our list of songs
+// function gotPageOfData(records, fetchNextPage) {
+//     console.log("gotPageOfData()");
+//     console.log("There are " + records.length + " items in records");
+//     // This takes the list of records and add them to the songs array
+//     image.push(...records);
+//     // request more pages
+//     fetchNextPage();
+//   }
+
+// // call back function that is called when ALL pages are loaded
+// function gotAllData(err) {
+//     console.log("gotAllData()");
+
+
+
+//   // call function to show the data
+//   showData();
+// }
+
+// // show the data on the page
+// function showData() {
+//     console.log("showData()");}
+
+// // find the shelf element
+// const imageContainer = document.querySelector("#container");
+
+// // loop through all the people listed in the Airtable data. Inside is the code we are applying for EACH person in the list of people.
+// scenes.forEach((scenes) => {
+//     // Print out what a single songs's data feidls looks like
+//     console.log("SHOWING THE SCENES");
+//     console.log(scenes.fields);})
+
+//     /** CREATE CONTAINER */
+//     const scenesContainer = document.createElement("div");
+//     scenesContainer.classList.add("scenesStyleContainer");
+
+//     /*******************
+//     ADD THE Environment
+//     *******************/
+
+//     const environmentElement = document.createElement("p");
+//     environmentElement.innerText = scenes.fields.Environment;
+//     scenesContainer.appendChild(environmentElement);
+
+//     /*******************
+//     ADD Angle
+//     *******************/
+
+//     const angleElement = document.createElement("p");
+//     angleElement.innerText = scenes.fields.angle;
+//     scenesContainer.appendChild(angleElement);
+
+//     /*******************
+//     ADD number of victim
+//     *******************/
+
+//     let victimElement = document.createElement("p");
+//     victimElement.innerText = "Victim: " + scenes.fields.victim;
+
+//     scenesContainer.appendChild(victimElement);
+
+
+
+//     /***********
+//      TODO: CREATE FILTER-BY-GENRE FUNCTIONALITY
+//      **********/
+//     let ScenesElement = scenes_image.cloneNode(true)
+//     albumArtELement.classList.add("scenes")
+//     console.log(document)
     
