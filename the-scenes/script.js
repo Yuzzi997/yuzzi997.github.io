efdb85a1b03ab9895b514b00178741cf9a7ee223
Yuzzi997 <zhuy700@newschool.edu>
2022-04-25 16:27:15 -0400

    last update

--- the-scenes/script.js
+++ the-scenes/script.js
@@ -6,13 +6,13 @@ var base = new Airtable({apiKey: 'keyN5uN05SQ6KpI6g'}).base('appKxL9aU5QyWGFe6')
 
 base('scenes').select({
     // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
+    maxRecords: 106,
     view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
-        console.log('Retrieved', record.get('Scenes'));
+        console.log('Retrieved', record.get('image'));
     });
 
     // To fetch the next page of records, call `fetchNextPage`.
@@ -20,81 +20,65 @@ base('scenes').select({
     // If there are no more records, `done` will get called.
     fetchNextPage();
 
-// }, function done(err) {
-//     if (err) { console.error(err); return; }
-// });
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
 
-// // an empty array to hold our people data
-// let scenes = [];
+// Get the "songs" table from the base, specify the view to be "View 2" (which is FILTERED for indie songs and SORTED by rating) and specify the callback functions that will receive each page of data
+base("scenes")
+  .select({
+    // add your view in here
+    view: "Grid view",
+  })
+  .eachPage(gotPageOfData, gotAllData);
+
+// an empty array to hold our people data
+let scenes = [];
+
 
 // callback function that receives each page of data (considered here as records) and adds them to our list of songs
-// function gotPageOfData(records, fetchNextPage) {
-//     console.log("gotPageOfData()");
-//     console.log("There are " + records.length + " items in records");
-//     // This takes the list of records and add them to the songs array
-//     image.push(...records);
-//     // request more pages
-//     fetchNextPage();
-//   }
-
-// // call back function that is called when ALL pages are loaded
-// function gotAllData(err) {
-//     console.log("gotAllData()");
-
-
-
-//   // call function to show the data
-//   showData();
-// }
-
-// // show the data on the page
-// function showData() {
-//     console.log("showData()");}
-
-// // find the shelf element
-// const imageContainer = document.querySelector("#container");
-
-// // loop through all the people listed in the Airtable data. Inside is the code we are applying for EACH person in the list of people.
-// scenes.forEach((scenes) => {
-//     // Print out what a single songs's data feidls looks like
-//     console.log("SHOWING THE SCENES");
-//     console.log(scenes.fields);})
-
-//     /** CREATE CONTAINER */
-//     const scenesContainer = document.createElement("div");
-//     scenesContainer.classList.add("scenesStyleContainer");
-
-//     /*******************
-//     ADD THE Environment
-//     *******************/
-
-//     const environmentElement = document.createElement("p");
-//     environmentElement.innerText = scenes.fields.Environment;
-//     scenesContainer.appendChild(environmentElement);
-
-//     /*******************
-//     ADD Angle
-//     *******************/
-
-//     const angleElement = document.createElement("p");
-//     angleElement.innerText = scenes.fields.angle;
-//     scenesContainer.appendChild(angleElement);
-
-//     /*******************
-//     ADD number of victim
-//     *******************/
-
-//     let victimElement = document.createElement("p");
-//     victimElement.innerText = "Victim: " + scenes.fields.victim;
-
-//     scenesContainer.appendChild(victimElement);
-
-
-
-//     /***********
-//      TODO: CREATE FILTER-BY-GENRE FUNCTIONALITY
-//      **********/
-//     let ScenesElement = scenes_image.cloneNode(true)
-//     albumArtELement.classList.add("scenes")
-//     console.log(document)
+function gotPageOfData(records, fetchNextPage) {
+    console.log("gotPageOfData()");
+    console.log("There are " + records.length + " items in records");
+    // This takes the list of records and add them to the songs array
+    scenes.push(...records);
+    // request more pages
     
+    fetchNextPage();
+  }
+
+// call back function that is called when ALL pages are loaded
+function gotAllData(err) {
+    console.log("gotAllData()");
+  
+    // report an error, you'd want to do something better than this in production
+    if (err) {
+      console.log("error loading data");
+      console.error(err);
+      return;
+    }
+  
+    // call function to show the data
+    showData();
+  }
+
+   
+//    scenes.forEach((image) => {
+//      // Print out what a single songs's data fields looks like
+//      console.log("SHOWING THE IMAGE");
+//      console.log(image.fields);
+
+    //  /** CREATE CONTAINER */
+    //  const imageContainer = document.createElement("div");
+    //  imageContainer.classList.add("imageContainer");
+
+
+    //   /*******************
+    //  ADD COVER ART
+    // *******************/
+    // let imageElement = imageEl.cloneNode(true)
+    // imageElement.classList.add("image")
+    //  //console.log(document)
+    //  imageElement.src = scenes.fields.image[0].url;
+    //  imageArtElement.innerText = scenes.fields.image;
+    //  imageContainer.appendChild(imageElement);
