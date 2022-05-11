f72bf8dba89ed23a36aa2b20a738668aae639410
Yuzzi997 <zhuy700@newschool.edu>
2022-05-11 01:06:54 -0400

    seatrip

---
+++ SeaTour/script.js
@@ -0,0 +1,233 @@
+// document.getElementsByClassName('myaudios');
+// consle.log("getall");
+
+var img1 = document.querySelector("#pic1");
+console.log("img1");
+var soundtrack1 = document.querySelector("#music1");
+var stop = false 
+img1.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack1.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack1.pause()
+        stop = false
+    }
+   
+})
+
+
+var img2 = document.querySelector("#pic2");
+console.log("img2");
+
+var soundtrack2 = document.querySelector("#music2");
+var stop = false 
+
+img2.addEventListener('click',function(){
+    console.log('palysound2');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack2.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack2.pause()
+        stop = false
+    }
+   
+})
+
+var img3 = document.querySelector("#pic3");
+console.log(img3);
+// without "" in console is meant to get the info of
+// with "" in consle is to see if the value is working
+
+var soundtrack3 = document.querySelector("#music3");
+var stop = false 
+
+img3.addEventListener('click',function(){
+    console.log('palysound3');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack3.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack3.pause()
+        stop = false
+    }
+   
+})
+
+var img4 = document.querySelector("#pic4");
+console.log("img4");
+
+var soundtrack4 = document.querySelector("#music4");
+var stop = false 
+
+img4.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack4.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack4.pause()
+        stop = false
+    }
+   
+})
+
+var img5 = document.querySelector("#pic5");
+console.log("img5");
+
+var soundtrack5 = document.querySelector("#music5");
+var stop = false 
+
+img5.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack5.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack5.pause()
+        stop = false
+    }
+   
+})
+
+var img6 = document.querySelector("#pic6");
+console.log("img6");
+
+var soundtrack6 = document.querySelector("#music6");
+var stop = false 
+
+img6.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack6.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack6.pause()
+        stop = false
+    }
+   
+})
+
+var img7 = document.querySelector("#pic7");
+console.log("img7");
+
+var soundtrack7 = document.querySelector("#music7");
+var stop = false 
+
+img7.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack7.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack7.pause()
+        stop = false
+    }
+   
+})
+
+var img8 = document.querySelector("#pic8");
+console.log("img8");
+
+var soundtrack8 = document.querySelector("#music8");
+var stop = false 
+
+img8.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack8.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack8.pause()
+        stop = false
+    }
+   
+})
+
+var img9 = document.querySelector("#pic9");
+console.log("img9");
+
+var soundtrack9 = document.querySelector("#music9");
+var stop = false 
+
+img9.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack9.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack9.pause()
+        stop = false
+    }
+   
+})
+
+var img10 = document.querySelector("#pic10");
+console.log("img10");
+
+var soundtrack10 = document.querySelector("#music10");
+var stop = false 
+
+img10.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack10.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack10.pause()
+        stop = false
+    }
+   
+})
+
+var img11 = document.querySelector("#pic11");
+console.log("img11");
+
+var soundtrack11 = document.querySelector("#music11");
+var stop = false 
+
+img11.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack11.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack11.pause()
+        stop = false
+    }
+   
+})
+
+var img12 = document.querySelector("#pic12");
+console.log("img12");
+
+var soundtrack12 = document.querySelector("#music12");
+var stop = false 
+
+img12.addEventListener('click',function(){
+    console.log('work');
+    // soundtrack.play();
+    if(stop == false){
+        soundtrack12.play();
+        stop = true
+    } else if(stop == true) {
+        soundtrack12.pause()
+        stop = false
+    }
+   
+})
+
+
